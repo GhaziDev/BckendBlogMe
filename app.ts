645a7f196f83d6d0fa7bd5c6015cce3ccf4879ec
GhaziGithub <gqj3893@autuni.ac.nz>
2024-11-13 23:45:40 +1300

    initial project setup

---
+++ app.ts
@@ -0,0 +1,16 @@
+
+
+import { Express } from "express"
+
+
+import { BodyParser } from "body-parser"
+
+import { SessionOptions } from "express-session"
+import { Session } from "express-session"
+
+//import {pool} from './routes/routes'
+
+
+
+import dotenv
+
